a86edf41bf84f5a01ef3d833069fc2ed8ae58070
cosmincodrea <codrea.cosmin27@gmail.com>
2026-02-20 12:54:02 +0200

    Move AuthRepository binding from common module to platform modules

--- composeApp/src/iosMain/kotlin/com/handy/di/PlatformModule.kt
+++ composeApp/src/iosMain/kotlin/com/handy/di/PlatformModule.kt
@@ -1,9 +1,9 @@
 package com.handy.di
 
+import com.handy.auth.AuthRepository
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module = module {
-    // iOS-specific bindings go here.
-    // AuthRepository is registered in the common authModule using the actual class.
+    single { AuthRepository(get()) }
 }
