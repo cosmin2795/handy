b086a2e6e60c236b81631c15e36f6b76fe8941de
cosmincodrea <codrea.cosmin27@gmail.com>
2026-02-23 18:13:16 +0200

    fix crash

--- composeApp/src/commonMain/kotlin/com/handy/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/handy/di/AppModule.kt
@@ -5,7 +5,12 @@ import com.handy.feature.auth.data.remote.AuthApi
 import com.handy.feature.auth.domain.repository.AuthRepository
 import com.handy.feature.auth.ui.AuthViewModel
 import com.handy.feature.home.ui.HomeViewModel
-import com.handy.network.createHttpClient
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.compose.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -13,7 +18,16 @@ import org.koin.dsl.module
 expect val platformModule: Module
 
 val networkModule = module {
-    single { createHttpClient() }
+    single {
+        HttpClient {
+            install(ContentNegotiation) {
+                json(Json { ignoreUnknownKeys = true })
+            }
+            install(Logging) {
+                level = LogLevel.INFO
+            }
+        }
+    }
     single { AuthApi(get()) }
 }
 
